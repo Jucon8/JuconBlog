2310b730b8895e2c100a9647bc87bbe8ea317863
Juan José Condinanzi <jucon8@gmail.com>
2020-07-07 21:22:33 -0300

    Santi-Bobadilla: View Noticia Integrada

--- resources/views/noticia.blade.php
+++ resources/views/noticia.blade.php
@@ -1,23 +1,23 @@
 
-
 @extends('layout')
 @section('titulo')
-Noticia:{{$posteo->id}}
+Noticia:
 @endsection
 
 @section('contenido')
   <!-- Page Header -->
+    
   <header class="masthead" style="background-image: url('{{url('img/corona.jpeg')}}')">
     <div class="overlay"></div>
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
           <div class="post-heading">
-              {{-- {{dd($posteo)}} --}}
-          <h1>{{$posteo->titulo}}</h1>
-            <h2 class="subheading">{{$posteo->comentario}}</h2>
+              @dd($posteo)
+          <h1>{{$posteo['title']}}</h1>
+            <h2 class="subheading">{{$posteo['comentario']}}</h2>
             <span class="meta">Publicado por:
-            <a href="#">{{$posteo->usuario}}</a>
+            <a href="#">{{$posteo['usuario']}}</a>
             {{$posteo->added_on->diffForHumans()}}</span>
           </div>
         </div>
